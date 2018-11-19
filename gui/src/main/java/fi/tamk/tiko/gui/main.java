42d6f57260d37dc1e78fd4338a1c27612bd66268
Unknown <toni.vanttinen@cs.tamk.fi>
2018-11-19 13:54:22 +0200

    Main javadoc
    Add: main javadoc

--- gui/src/main/java/fi/tamk/tiko/gui/main.java
+++ gui/src/main/java/fi/tamk/tiko/gui/main.java
@@ -2,6 +2,16 @@ package fi.tamk.tiko.gui;
 
 import javafx.application.Application;
 
+/**
+ * Main Class
+ *
+ * Used to launch the UI
+ *
+ * @author Toni Vänttinen (toni.vanttinen@cs.tamk.fi)
+ * @version V1
+ * @since 2018-11-19
+ */
+
 public class main {
     public static void main(String[] args) {
         Application.launch(Gui.class, args);
