719ca8754170e4e97ca905faa167222eef7407b9
Unknown <toni.vanttinen@cs.tamk.fi>
2018-12-17 18:47:08 +0200

    Dropbox file saver Javadoc
    Dropbox file saver Javadoc

--- jsonparser/src/main/java/fi/tamk/tiko/jsonparser/DropboxSaver.java
+++ jsonparser/src/main/java/fi/tamk/tiko/jsonparser/DropboxSaver.java
@@ -9,9 +9,30 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * Dropbox class
+ * Used to save files to dropbox
+ *
+ * @author Toni Vänttinen (toni.vanttinen@cs.tamk.fi)
+ * @version V1
+ * @since 2018-12-17
+ */
 public class DropboxSaver {
+    /**
+     * Access token where the file is saved
+     */
     private static final String ACCESS_TOKEN = "oY9YcCe30dAAAAAAAAAAaRjZBNEBUfzyZJ5xLhbwDaBehAW_cPkZ-fl5VsjbH1JC";
 
+    /**
+     * Saves the file to dropbox
+     *
+     * First the dropbox client is accessed
+     * and then the selected file is saved there
+     *
+     * @param fileName file that is to be saved
+     * @throws DbxException dropbox error
+     * @throws IOException run error
+     */
     public static void save(String fileName) throws DbxException, IOException {
 
         DbxRequestConfig config = new DbxRequestConfig("dropbox/java-tutorial", "en_US");
